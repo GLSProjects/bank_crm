e1b701777d74c3830860465651771782eb579bc0
jeet <jeetkhanpara1412@gmail.com>
2026-02-20 09:27:17 +0530

    add-navbar

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -132,6 +132,7 @@ const Card = ({ title, value }) => (
 const chartBox = {
   flex: 1,
   background: "#fff",
+  
   padding: "20px",
   borderRadius: "10px",
   boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
